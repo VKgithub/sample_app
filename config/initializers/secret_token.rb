fc9df34a8a86aebe444978de205ede544f1b0ab1
SaVaGe <svg@linuxmint.linuxmint-domain>
2012-03-15 20:55:31 +0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'aa7495176f056e5df8a5191003f174f43f3ecb3cccc5d1d034abcd42d42af7a712802603e698ce31acbf0e9d3f5a3976440e8eb9d92a607724bcfc66da069826'
